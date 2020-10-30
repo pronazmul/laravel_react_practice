60890386b033f0336213469eca01ed9db4140938
Nazmul Huda <mhnazmul48@gmail.com>
2020-10-30 19:01:10 +0600

    REact Setup Done

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-require('./bootstrap');
+import React from 'react'
+import ReactDom from 'react-dom'
+
+ReactDom.render(
+    <React.StrictMode>
+            <h1>Hello World I am from React World</h1>
+    </React.StrictMode>
+)
+
+document.getElementById('root')
